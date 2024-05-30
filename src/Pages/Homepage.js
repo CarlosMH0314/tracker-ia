92bfe2509a4c3eeddd4108064d1f8585884542a2
CarlosMH0314 <a21311070@uthermosillo.edu.mx>
2024-05-30 16:46:56 -0700

    Consumo de Api externa, Creaccion de vista Homepage, Diseño de la vista principal

--- src/Pages/Homepage.js
+++ src/Pages/Homepage.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
+import Banner from "../components/Banner/Banner";
+import CoinsTable from "../components/CoinsTable";
 
 const Homepage = () => {
   return (
-    <div>Homepage</div>
-  )
-}
+    <>
+      <Banner />
+      <CoinsTable />
+    </>
+  );
+};
 
-export default Homepage
+export default Homepage;
---
+++ src/components/Banner/Carousel.js
@@ -0,0 +1,97 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import AliceCarousel from "react-alice-carousel";
+import { Link } from "react-router-dom";
+import { TrendingCoins } from "../../config/api";
+import { useCrypto } from "../../CryptoContext";
+import { numberWithCommas } from "../CoinsTable";
+import { styled } from "@mui/system";
+
+const CarouselContainer = styled('div')(({ theme }) => ({
+  height: "50%",
+  display: "flex",
+  alignItems: "center",
+}));
+
+const CarouselItem = styled(Link)(({ theme }) => ({
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  cursor: "pointer",
+  textTransform: "uppercase",
+  color: "white",
+  textDecoration: "none", // Add this to remove the default link underline
+}));
+
+const Carousel = () => {
+  const [trending, setTrending] = useState([]);
+  const { currency, symbol } = useCrypto();
+
+  const fetchTrendingCoins = async () => {
+    const { data } = await axios.get(TrendingCoins(currency));
+    setTrending(data);
+  };
+
+  useEffect(() => {
+    fetchTrendingCoins();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [currency]);
+
+  const items = trending.map((coin) => {
+    let profit = coin?.price_change_percentage_24h >= 0;
+
+    return (
+      <CarouselItem to={`/coins/${coin.id}`} key={coin.id}>
+        <img
+          src={coin?.image}
+          alt={coin.name}
+          height="80"
+          style={{ marginBottom: 10 }}
+        />
+        <span>
+          {coin?.symbol}
+          &nbsp;
+          <span
+            style={{
+              color: profit > 0 ? "rgb(14, 203, 129)" : "red",
+              fontWeight: 500,
+            }}
+          >
+            {profit && "+"}
+            {coin?.price_change_percentage_24h?.toFixed(2)}%
+          </span>
+        </span>
+        <span style={{ fontSize: 22, fontWeight: 500 }}>
+          {symbol} {numberWithCommas(coin?.current_price.toFixed(2))}
+        </span>
+      </CarouselItem>
+    );
+  });
+
+  const responsive = {
+    0: {
+      items: 2,
+    },
+    512: {
+      items: 4,
+    },
+  };
+
+  return (
+    <CarouselContainer>
+      <AliceCarousel
+        mouseTracking
+        infinite
+        autoPlayInterval={1000}
+        animationDuration={1500}
+        disableDotsControls
+        disableButtonsControls
+        responsive={responsive}
+        items={items}
+        autoPlay
+      />
+    </CarouselContainer>
+  );
+};
+
+export default Carousel;
